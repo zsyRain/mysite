30f6fc10cd1832cd33b4d8b5b894adf000b10450
zsyRain <709894911@qq.com>
2018-07-29 06:19:39 +0800

    7.29

--- account/views.py
+++ account/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate,login
+from .forms import LoginForm
 
 # Create your views here.
 def user_login(request):
-    return render(request, "blog/titles.html", {"blogs": blogs})
+    if request.method == "POST":
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            cd = login_form.cleaned_data
+            user = authenticate(username=cd['username'],password=cd['passwor'])
+
+            if user:
+                login(request,user)
+                return HttpResponse("Welcome You,You have been authenticated successfully")
+            else:
+                return HttpResponse("Invalid login")
+
+        if request.method == "GET":
+            login_form = LoginForm()
+            return render(request,"account/login.html",{"form":login_form})
 
