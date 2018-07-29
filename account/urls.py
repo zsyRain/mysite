2fbeee632bb396c2e07936625c0720c50d9280fe
zsyRain <709894911@qq.com>
2018-07-29 23:28:33 +0800

    7.29

--- account/urls.py
+++ account/urls.py
@@ -8,7 +8,11 @@ from django.contrib.auth import views as  auth_views
 urlpatterns = [
     #url('^login/$',views.user_login,name="user_login"),     #自定义的登录
     #url('^login/$',auth_views.login,name="user_login"),  #django内置的登录
-    url('^new-login/$',auth_views.login,{"template_name":"account/login.html"},name="user_login"),   #django内置的登录
+    url('^login/$',auth_views.login,{"template_name":"account/login.html"},name="user_login"),   #django内置的登录
     url('^logout/$',auth_views.logout,{"template_name":"account/logout.html"},name="user_logout"),   #django内置的退出
-    url('^register/$',views.register,name="user_register")
+    url('^register/$',views.register,name="user_register"),
+
+    url('^password-change/$',auth_views.password_change,{"post_change_redirect":"/account/password-change-done"},name='password_change'),
+    url('^password-change-done/$',auth_views.password_change_done,name='password_change_done'),
+
 ]
