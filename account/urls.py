9cc10d9160a6513bf9c99f20018e8ac53f6f11c5
Administrator <709894911@qq.com>
2018-07-29 14:24:41 +0800

    7.29

--- account/urls.py
+++ account/urls.py
@@ -8,5 +8,7 @@ from django.contrib.auth import views as  auth_views
 urlpatterns = [
     #url('^login/$',views.user_login,name="user_login"),     #自定义的登录
     #url('^login/$',auth_views.login,name="user_login"),  #django内置的登录
-    url('^new-login/$',auth_views.login,{"template_name":"account/login.html"}),   #django内置的登录
+    url('^new-login/$',auth_views.login,{"template_name":"account/login.html"},name="user_login"),   #django内置的登录
+    url('^logout/$',auth_views.logout,{"template_name":"account/logout.html"},name="user_logout"),   #django内置的退出
+    url('^register/$',views.register,name="user_register")
 ]
