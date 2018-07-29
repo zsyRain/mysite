e993d220177e41988dbc3cfe3b890a2d8a8c5426
Administrator <709894911@qq.com>
2018-07-29 10:25:59 +0800

    7.29

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 from django.conf import settings
 
+from django.contrib.auth import views as  auth_views
+
 urlpatterns = [
-    #url('^login/$',views.user_login,name="user_login"),   //自定义的登录
-    url('^login/$',auth_views.login,name="user_login"),
+    #url('^login/$',views.user_login,name="user_login"),     #自定义的登录
+    url('^login/$',auth_views.login,name="user_login"),  #django内置的登录
 ]
